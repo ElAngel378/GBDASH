ee07ab8b1af1d874be4d376124b6fa81497ab83e
Soteris24 <66694845+Soteris24@users.noreply.github.com>
2026-09-02 18:12:18 +0300

    Add centered 32x32 play button sprite to main menu

--- src/state_menu.c
+++ src/state_menu.c
@@ -57,6 +57,35 @@ GameState update_menu_state(void) {
     // Title 
     draw_text(0, 1, "GEOMETRY DASH POCKET");
 
+    // Load Play Button (4x4 tiles / 8 8x16 sprites)
+    extern const unsigned char playbutton[];
+    // Tiles 1-16 (skip tile 0 which is empty)
+    set_sprite_data(0, 16, &playbutton[16]);
+
+    if (_cpu == CGB_TYPE) {
+        static const uint16_t play_button_palette[] = {
+            RGB8(255, 255, 255), // Trans
+            RGB8(138, 245, 30),  // Light Green
+            RGB8(30, 140, 20),   // Dark Green
+            RGB8(0, 0, 0)        // Black Outline
+        };
+        set_sprite_palette(0, 1, play_button_palette);
+    }
+
+    SPRITES_8x16;
+    uint8_t bx = 72; // Centered X (64 + 8)
+    uint8_t by = 68; // Centered Y (52 + 16)
+
+    // Set 8 8x16 hardware sprites for the 32x32 button
+    set_sprite_tile(0, 0);  move_sprite(0, bx, by);           set_sprite_prop(0, 0);
+    set_sprite_tile(1, 2);  move_sprite(1, bx, by + 16);      set_sprite_prop(1, 0);
+    set_sprite_tile(2, 4);  move_sprite(2, bx + 8, by);       set_sprite_prop(2, 0);
+    set_sprite_tile(3, 6);  move_sprite(3, bx + 8, by + 16);  set_sprite_prop(3, 0);
+    set_sprite_tile(4, 8);  move_sprite(4, bx + 16, by);      set_sprite_prop(4, 0);
+    set_sprite_tile(5, 10); move_sprite(5, bx + 16, by + 16); set_sprite_prop(5, 0);
+    set_sprite_tile(6, 12); move_sprite(6, bx + 24, by);      set_sprite_prop(6, 0);
+    set_sprite_tile(7, 14); move_sprite(7, bx + 24, by + 16); set_sprite_prop(7, 0);
+
     bg_x = 0;
     ground_x = 0;
 
@@ -68,16 +97,20 @@ GameState update_menu_state(void) {
     enable_interrupts();
 
     SHOW_BKG;
+    SHOW_SPRITES;
     DISPLAY_ON;
 
     while (1) {
         SCX_REG = 0; // Header
 
-        if (joypad() & J_A) {
+        uint8_t joy = joypad();
+        if (joy & (J_A | J_START)) {
             waitpadup();
             disable_interrupts();
             remove_LCD(menu_stat_isr);
             set_interrupts(VBL_IFLAG | TIM_IFLAG);
+            HIDE_SPRITES;
+            for (uint8_t s = 0; s < 8; s++) hide_sprite(s);
             enable_interrupts();
             return STATE_LEVEL_SELECT;
         }
